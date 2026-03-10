e44cc49cd108e6c48c0e47da7f6850ee27d64c8f
henrynenuphar <hanryjames18@gmail.com>
2026-03-10 22:39:25 +0700

    fix: update base URL for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => ({
     plugins: [react()],
-    base: command === 'build' ? '/ENGO/' : '/',
+    base: '/ENGO-GITHUB/',
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
